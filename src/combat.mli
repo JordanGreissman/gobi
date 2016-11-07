9d5a21068846b1ebb77efcfabfc0add9cc93c1c7
Jordan Greissman <jag538@cornell.edu>
2016-11-07 18:12:13 -0500

    Changes to combat and json

--- src/combat.mli
+++ src/combat.mli
@@ -4,6 +4,10 @@ open cluster
  * attacking unit's weight in this instance of combat *)
 val compute_weights : int -> int -> int
 
+(* [attack_building] returns true if the attacking unit
+ * destroys the building, false otherwise *)
+val attack_building : int -> int -> bool
+
 (* [attack_unit] returns true if the attacking unit wins
  * and false otherwise *)
 val attack_unit : int -> int -> bool
