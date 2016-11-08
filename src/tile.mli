4d963e5e5a36bd24bf58b88be28db2963d3476ed
David Udelson <dru5@cornell.edu>
2016-11-08 14:27:00 -0500

    Squashed commit of the following:
    commit 733ab5678af94a05a98f04d99868ec2a95d6cf39
    Author: David Udelson <dru5@cornell.edu>
    Date:   Tue Nov 8 14:13:27 2016 -0500
    
        updated notes.txt after visit to OH
    
    commit ce3c8a067a70abfc7c8984febf942c4151e34e49
    Author: David Udelson <dru5@cornell.edu>
    Date:   Mon Nov 7 19:33:06 2016 -0500
    
        Added questions for OH to notes
    
    commit 18fda6f431ad42364a44e9d700fcdc8e2f0ef8a1
    Author: David Udelson <dru5@cornell.edu>
    Date:   Mon Nov 7 19:08:04 2016 -0500
    
        Resolved question about module type arguments
    
    commit 5d51389a3a987571cd0a64661960b2ccbb87a029
    Author: David Udelson <dru5@cornell.edu>
    Date:   Mon Nov 7 17:45:59 2016 -0500
    
        Updates to MLIs
    
    commit 595fe34fcffd363791ac330f68f037ec13706480
    Author: David Udelson <dru5@cornell.edu>
    Date:   Sun Nov 6 19:59:25 2016 -0500
    
        Playing around with mli structure

--- src/tile.mli
+++ src/tile.mli
@@ -1,55 +1,42 @@
-open Cluster
-open Hub
-open Entity
-
-include Identifiable
-include Describable
-
-module type Terrain = sig
-  (* the [info] type will be a record containing the following fields:
-   *  - movementObstruction: bool; whether units are allowed on this tile
-   *  - costToMove: int; the number of turns it takes unit to traverse this tile.
-   *      For tiles where [movementObstruction = true], [costToMove = -1].
-   *  - needsClearing: bool; whether this tile needs to be cleared before it can
-   *      be settled.
-   *  - buildingRestriction: bool; whether hubs are allowed on this tile
-   *  - foodRestriction: bool
-  *)
-  type info
-  type t =
-    | Flatland of info
-    | Mountain of info
-    | Forest of info
-    | Desert of info
-end
-
 (** the type of a tile *)
 type t
 
+(* the [info] type will be a record containing the following fields:
+ *  - movementObstruction: bool; whether units are allowed on this tile
+ *  - costToMove: int; the number of turns it takes unit to traverse this tile.
+ *      For tiles where [movementObstruction = true], [costToMove = -1].
+ *  - needsClearing: bool; whether this tile needs to be cleared before it can
+ *      be settled.
+ *  - buildingRestriction: bool; whether hubs are allowed on this tile
+ *  - foodRestriction: bool
+ *)
+type terrain_info
+
+(** a tile can have one of these terrain types *)
+type terrain =
+  | Flatland of terrain_info
+  | Mountain of terrain_info
+  | Forest of terrain_info
+  | Desert of terrain_info
 
 (* TODO: Should setters return some kind of indication of whether or not the
  * set operation was successful? What could go wrong? *)
 
-(* TODO: By taking [id] instead of [t], every function in this API is calling
- * into map.ml. Is that ok? *)
-
-val get_terrain : id -> terrain
-val set_terrain : id -> terrain -> unit
+val get_terrain : t -> terrain
+val set_terrain : t -> terrain -> unit
 
 (* [is_settled] returns bool indentified in tile given by it's [id] [settled]*)
-val is_settled : id -> bool
-val settle : id -> unit
-val unsettle : id -> unit
+val is_settled : t -> bool
+val settle : t -> unit
+val unsettle : t -> unit
 
 (* [get_hub] returns the type of hub associated with the tile given by
   * it's [id] *)
-val get_hub : id -> hub option
-val set_hub : id -> hub option -> unit
+val get_hub : t -> Hub.t option
+val set_hub : t -> Hub.t option -> unit
 
-(* [get_bits] returns the type of hub associated with the tile given by
-  * it's [id] *)
-val get_entity : id -> entity list
-val set_entity: id -> unit
+val get_entity : t -> entity list
+val set_entity: t -> unit
 
 (** [create_tile] returns a newly created tile with the given parameters *)
-val create_tile : id -> terrain -> bool -> hub option -> entity list -> tile
+val create_tile : t -> terrain -> bool -> hub option -> entity list -> tile
