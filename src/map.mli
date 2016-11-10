ed85bbea24852463c5102abff25acb87e4120a3f
David Udelson <dru5@cornell.edu>
2016-11-10 16:08:30 -0500

    Implemented coordinate systems

--- src/map.mli
+++ src/map.mli
@@ -1,27 +1,28 @@
 (** a data structure which stores all of the tiles in the game *)
 type t
 
-(** a coordinate type. We're using hexagonal axial coordinates, which means one
-  * coordinate axis is vertical, and the other is at a -30 degree angle to
-  * horizontal. Like this:
+(** This type represents a coordinate in a hexagonal grid. We're using offset
+  * coordinates -- a slight modification of cartesian coordinates to suit a
+  * hexagonal grid. They look like this:
   *   ____        ____
   *  /(0,0)      /    \
-  * / q  p \____/(1,-1)\
-  * \ |   \/    \      /
-  *  \|___/\(1,0)\____/
-  *  /|   \ \    /    \
-  * / v    \_>__/ (2,0)\
+  * /col   \____/(2,0) \
+  * \ | row/====\==>   /
+  *  \|___/ (1,0)\____/
+  *  /|   \      /    \
+  * / v    \____/ (2,1)\
   * \ (0,1)/    \      /
   *  \____/ (1,1)\____/
   *  /    \      /    \
-  * / (0,2)\____/ (2,1)\
+  * / (0,2)\____/ (2,2)\
   * \      /    \      /
   *  \____/      \____/
-  *
-  * The axes are called p and q so as to not confuse their properties with those
-  * of traditional (square) coordinate systems (mainly that they're orthogonal).
   *)
 type coordinate
 
 (** look up a tile by its coordinates *)
 val tile_by_pos : coordinate -> t -> Tile.t option
+
+(** procedurally generate a map. The first argument is the width of the map, and
+  * the second is the height *)
+val generate : int -> int -> t
