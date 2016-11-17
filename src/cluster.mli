e602ae4428a5ce9097d2bd1fe34c20a9a0d09fd5
Matt Barker <mjb485@cornell.edu>
2016-11-17 00:25:50 -0500

    forgot to add a file, what a CLUSTERfudge

--- src/cluster.mli
+++ src/cluster.mli
@@ -7,6 +7,6 @@
 (** the type of a cluster *)
 type t
 
-(** creates a new cluster based on existing units on a settled tile *)
-val create : Tile.t -> t
+(** creates a new cluster with a name on a settled tile *)
+val create : string -> Tile.t -> t
 
