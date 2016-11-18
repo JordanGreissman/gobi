eeadc251e2e5e686610fbe7936c2952af501b5aa
David Udelson <dru5@cornell.edu>
2016-11-18 18:35:40 -0500

    Rename Map to Mapp to avoid conflict with Ocaml stdlib

---
+++ src/mapp.ml
@@ -0,0 +1,11 @@
+type tile = Tile.t
+
+type t = tile list list
+
+(* map operations *)
+let tile_by_pos c map =
+  failwith "Unimplemented"
+
+(* map generation *)
+let generate width height =
+  failwith "Unimplemented"
