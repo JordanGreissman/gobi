eb38675b5c5107cdf90c20785bf2b0a513977129
Jordan Greissman <jag538@cornell.edu>
2016-11-29 14:50:54 -0500

    Random map generation and town hall placement for the game start

--- src/cluster.mli
+++ src/cluster.mli
@@ -4,4 +4,4 @@ type t
 
 (** creates a new cluster with a name on a settled tile *)
 val create : name:string -> descr:string -> town_hall_tile:Tile.t ->
-	hub_role_list:Hub.role list -> t
+	hub_role_list:Hub.role list -> map:Mapp.t -> t * Mapp.t
