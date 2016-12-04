93bcc7c038dcd4a44bf100a43b71540ecbdb71f7
Jordan Greissman <jag538@cornell.edu>
2016-12-03 20:59:15 -0500

    Interface displays resources

--- src/resource.ml
+++ src/resource.ml
@@ -26,18 +26,18 @@ let str_to_res str = match String.lowercase_ascii str with
  | _       -> None
 
 let res_to_str res = match res with
-	| Iron -> "iron" | Gold -> "gold"
-	| Paper -> "paper" | Food -> "food"
+	| Iron -> "Iron" | Gold -> "Gold"
+	| Paper -> "Paper" | Food -> "Food"
 	| _ -> ""
 
-let find_res res lst = 
+let find_res res lst =
 	let find tup = match tup with
 		| (kind, amt) when ((Some kind) = (str_to_res res)) -> true
 		| _ -> false in
-	try List.find find lst with 
+	try List.find find lst with
 		| Not_found -> raise (Exception.Illegal "can't find resource")
 
-let change_resource key amt lst = 
+let change_resource key amt lst =
 	let (res, the_amt) = find_res key lst in
 	let new_res = (res, the_amt + amt) in
 	let list_without = List.filter (fun (the_res, amt) -> not (the_res = res)) lst in
