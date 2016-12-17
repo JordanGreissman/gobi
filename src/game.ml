59b92526d8400baee46944e361a56cadeb9a1f51
David Udelson <dru5@cornell.edu>
2016-12-16 19:46:38 -0500

    Move json parser to its own module

--- src/game.ml
+++ src/game.ml
@@ -1,4 +1,4 @@
-open Yojson
+open Core.Std
 open CamomileLibrary
 open Civ
 open State
@@ -8,15 +8,6 @@ open LTerm_event
 
 type civ = Civ.t
 type menu = Menu.t
-
-type parsed_json = {
-  turns: int;
-  ai: int;
-  entities: Entity.role list;
-  hubs: Hub.role list;
-  civs: (string * string) list;
-  tech_tree: Research.Research.research_list;
-}
 
 type win_state = Military | Tech | Score | Tie | No_Win
 
@@ -29,94 +20,7 @@ let dispatch_message (state:State.t) s k =
   let m = Message.create s k in
   { state with messages = m::state.messages }
 
-(* Error handling necessary? *)
-let load_json s =
-  try Yojson.Basic.from_file s with
-  | _ -> raise (Critical ("game",
-                          "load_json",
-                          "File does not exist or is not a JSON"))
-
-let get_assoc s json =
-  json |> Yojson.Basic.Util.member s
-  |> Basic.Util.to_list |> Basic.Util.filter_assoc
-
-let extract_list str lst =
-  let json_list = List.assoc str lst |> Basic.Util.to_list in
-  Basic.Util.filter_map (Basic.Util.to_string_option) json_list
-
-let extract_game assoc =
-  let turns = (List.assoc "turns" assoc) |> Basic.Util.to_int in
-  let ai = (List.assoc "ai" assoc) |> Basic.Util.to_int in
-  (turns, ai)
-
-let extract_techs entity_role_list hub_role_list assoc =
-  let tech = (List.assoc "tech" assoc) |> Basic.Util.to_string in
-  let resource = (List.assoc "resource" assoc) |> Basic.Util.to_string in
-  let cost = (List.assoc "cost" assoc) |> Basic.Util.to_int in
-  let treasure = (List.assoc "treasure" assoc) |> Basic.Util.to_list
-                 |> Basic.Util.filter_assoc in
-  let treasure = List.nth treasure 0 in
-  let hub = (List.assoc "hub" treasure) |> Basic.Util.to_string in
-  let amount = (List.assoc "amount" treasure) |> Basic.Util.to_int in
-  let entity = extract_list "entity" treasure in
-  Research.Research.extract_to_value tech resource cost hub amount
-    entity entity_role_list hub_role_list
-
-let extract_unlockable entity_role_list hub_role_list assoc =
-  let branch = (List.assoc "branch" assoc) |> Basic.Util.to_string in
-  let techs = (List.assoc "techs" assoc) |> Basic.Util.to_list
-              |> Basic.Util.filter_assoc in
-  let techs = List.map (extract_techs entity_role_list hub_role_list) techs in
-  (branch, techs)
-
-let extract_hub entity_role_list assoc =
-  let name = (List.assoc "name" assoc) |> Basic.Util.to_string in
-  let desc = (List.assoc "desc" assoc) |> Basic.Util.to_string in
-  let builder = (List.assoc "built by" assoc) |> Basic.Util.to_string in
-  let defense = (List.assoc "defense" assoc) |> Basic.Util.to_int in
-  let cost = (List.assoc "cost" assoc) |> Basic.Util.to_int in
-  let entities = extract_list "entities" assoc in
-  let generates = (List.assoc "generates" assoc) |> Basic.Util.to_list
-                  |> Basic.Util.filter_assoc in
-  let generates = List.nth generates 0 in
-  let resource = (List.assoc "resource" generates) |> Basic.Util.to_string in
-  let amount = (List.assoc "amount" generates) |> Basic.Util.to_int in
-  Hub.extract_to_role name desc builder defense cost
-    resource amount entities entity_role_list
-
-let extract_civ assoc =
-  let name = (List.assoc "name" assoc) |> Basic.Util.to_string in
-  let desc = (List.assoc "desc" assoc) |> Basic.Util.to_string in
-  (name, desc)
-
-let extract_entity assoc =
-  let name = (List.assoc "name" assoc) |> Basic.Util.to_string in
-  let desc = (List.assoc "desc" assoc) |> Basic.Util.to_string in
-  let attack = (List.assoc "attack" assoc) |> Basic.Util.to_int in
-  let defense = (List.assoc "defense" assoc) |> Basic.Util.to_int in
-  let actions = (List.assoc "actions" assoc) |> Basic.Util.to_int in
-  let cost = (List.assoc "cost" assoc) |> Basic.Util.to_int in
-  let requires = (List.assoc "requires" assoc) |> Basic.Util.to_string in
-  Entity.extract_to_role name desc requires cost attack defense actions
-
-let init_json json =
-  let meta = json |> Yojson.Basic.Util.member "game"
-             |> Basic.Util.to_assoc |> extract_game in
-  let entities = List.map extract_entity
-      (get_assoc "entities" json) in
-  let hubs = List.map (extract_hub entities)
-      (get_assoc "hubs" json) in
-  let unlockables = List.map (extract_unlockable entities hubs)
-      (get_assoc "techtree" json) in
-  let branches = List.map fst unlockables in
-  let techs = List.map snd unlockables in
-  let tree = Research.Research.create_tree branches techs [] in
-  let civs = List.map extract_civ
-      (get_assoc "civilizations" json) in
-  {turns = fst meta; ai = snd meta; entities = entities; hubs = hubs;
-   tech_tree = tree; civs = civs}
-
-let init_civ player_controlled parsed map unlocked civ : civ =
+let init_civ player_controlled (parsed:Parser.t) map unlocked civ : civ =
   let entity_roles = parsed.entities in
   let hub_roles = parsed.hubs in
   let tech_tree = parsed.tech_tree in
@@ -163,8 +67,7 @@ let get_player_start_coords civs =
   (c, Coord.Screen.add offset origin)
 
 let init_state json : State.t =
-  let json = Basic.from_file json in
-  let parsed = init_json json in
+  let parsed = Parser.init_json json in
   let map = ref (Mapp.generate 42 42) in
   let unlocked =
     List.map
