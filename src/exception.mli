f1ee8c9e979664f2601e5106ccc33a3a87ea5f84
David Udelson <dru5@cornell.edu>
2016-12-30 19:50:25 -0500

    Test telegram integration

--- src/exception.mli
+++ src/exception.mli
@@ -1,7 +1,8 @@
 (** Used for non-critical errors which can be printed to the messages pane, for
   * example, trying to place a hub on a tile that already contains a hub. Usually
   * these errors are illegal actions within the game. The sole argument is the
-  * error message to be displayed in the messages pane. *)
+  * error message to be displayed in the messages pane.
+  * test *)
 exception Illegal of string
 
 (** Used for critical errors which halt program execution, such as failure to
